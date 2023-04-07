4efb899d84fca50e70da5cccf3479d492a89d4c3
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-04-06 23:00:58 -0600

    me perdi (º～º)

--- hdt8-algoyestructuras/src/project/main.java
+++ hdt8-algoyestructuras/src/project/main.java
@@ -10,8 +10,15 @@ public class main {
 		System.out.println("| Bienvenido a mi Hospital |");
 		System.out.println("+--------------------------+");
 		System.out.println();
-		Heap<Patients> Tree = new Heap();
+		int op1 = 0;
 		
+		System.out.println("Elija una implementacion de Heap: \n 1. Custom Heap (mio :D) \n 2. Priority Queue JavaFramework");
+		op1 = tec.nextInt();
+		
+	}
+	
+	public Heap customHeap() {
+		Heap<Patients> Tree = new Heap();
 		try {
 			File myFile = new File("datos.txt");
 			Scanner myReader = new Scanner(myFile);
@@ -30,6 +37,32 @@ public class main {
 			System.out.println("An error occurred.");
 		      e.printStackTrace();
 		}
+		
+		return Tree;
+	}
+	
+	public PriorityQueue JavaFrameworksHeap() {
+		PriorityQueue<Patients> Tree = new PriorityQueue();
+		try {
+			File myFile = new File("datos.txt");
+			Scanner myReader = new Scanner(myFile);
+			while (myReader.hasNextLine()) {
+		        String[] data = myReader.nextLine().split(",");
+		        for(int i = 0; i > 3; i++) {
+		        	data[i] = data[i].trim();
+		        }
+		        
+		        Patients newPatient = new Patients(data[0],data[1],data[2]);
+		        Tree.add(newPatient);
+		        System.out.println(newPatient.toString());
+		      }
+			myReader.close();			
+		} catch (FileNotFoundException  e) {
+			System.out.println("An error occurred.");
+		      e.printStackTrace();
+		}
+		
+		return Tree;
 	}
 
 }
